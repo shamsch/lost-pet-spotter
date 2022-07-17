7c2edfad3937444cae7713d19fcc7ed23d84dcc8
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-07-17 17:32:03 +0300

    dummy post for now

--- src/screens/AllPost.tsx
+++ src/screens/AllPost.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
+import PostList from '../components/PostList'
+import { DUMMY_POSTS } from '../utils/constant'
 
 const AllPost = () => {
   return (
-    <View>
-      <Text>AllPost</Text>
-    </View>
+    <PostList
+      posts={DUMMY_POSTS}
+    />
   )
 }
 

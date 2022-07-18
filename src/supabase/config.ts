133249853d6254ecbd73ed5798217d842a4e08e8
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-07-18 12:54:16 +0300

    env variable set up for production

--- src/supabase/config.ts
+++ src/supabase/config.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import Constants from "expo-constants";
 
-const supabaseUrl = Constants?.manifest?.extra?.SUPABASE_API;
-const supabaseAnonKey = Constants?.manifest?.extra?.SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_API || Constants?.manifest?.extra?.SUPABASE_API;
+const supabaseAnonKey = process.env.SUPABASE_KEY || Constants?.manifest?.extra?.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- src/utils/googleMapStatic.ts
+++ src/utils/googleMapStatic.ts
@@ -1,7 +1,6 @@
 import Constants from "expo-constants";
-import { LongPressGestureHandler } from "react-native-gesture-handler";
 
-const API_KEY = Constants.manifest?.extra?.API_KEY;
+const API_KEY = process.env.GOOGLE_API_KEY || Constants.manifest?.extra?.API_KEY;
 
 export const getStaticMapUrl = (latitude: number, longitude: number) => {
 	const url = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x220&maptype=roadmap
